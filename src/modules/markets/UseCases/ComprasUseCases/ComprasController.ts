824b9f5e6106526334ac2218d049d72bfc13b8db
ettore <ettore.barbosa@gmail.com>
2022-03-18 15:54:43 -0300

    babel config

--- src/modules/markets/UseCases/ComprasUseCases/ComprasController.ts
+++ src/modules/markets/UseCases/ComprasUseCases/ComprasController.ts
@@ -4,7 +4,7 @@ import { ComprasUseCase } from "./ComprasUsecase";
 
 export class ComprasController {
   async handle(req: Request, res: Response): Promise<Response> {
-    const comprasUseCase = container.resolve(ComprasUseCase)
+    const comprasUseCase = container.resolve(ComprasUseCase);
 
     const compras = await comprasUseCase.execute();
     return res.json(compras)
